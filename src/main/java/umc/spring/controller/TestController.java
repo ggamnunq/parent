39b0b15fc34334e9090b234b6730ef7dee086dcd
junyong <rlawnsdyd530@gmail.com>
2025-03-14 01:51:38 +0900

    feat : 컨트롤러 변경

--- src/main/java/umc/spring/controller/TestController.java
+++ src/main/java/umc/spring/controller/TestController.java
@@ -1,8 +1,14 @@
 package umc.spring.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 
 }
